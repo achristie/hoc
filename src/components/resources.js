3faf6a1964d29a8658e276332918109ebf4b03cd
Andrew Christie <awchristie86@gmail.com>
2016-07-04 18:00:31 -0400

    added naive auth

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special
+      <ul>
+        <li>Andrew!</li>
+        <li>Yes!</li>
+        <li>No!</li>
+      </ul>
+    </div>
+  );
+}
